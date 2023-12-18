df6e12df274e25929b07e763ea6979dafcb8bbdc
gerel <gerel@bk.ru>
2023-12-18 17:58:25 +0500

    Final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// Задача 3: Задайте произвольную строку.
 //Выясните, является ли она палиндромом.
+string str = "шалаjш";
+void IsPalindrom(string stroka)
+{
+    str = stroka.ToLower();
+    bool palindrom = true;
+    for (int i = 0; i < str.Length/2; i++)
+    {
+        if (str[i] != str[str.Length-i-1])
+        {
+            palindrom = false;
+            break;
+        }
+    }
+    if (palindrom) Console.WriteLine($"Строка '{str}' является палиндромом");
+    else Console.WriteLine($"Строка '{str}' не является палиндромом");
+}
+IsPalindrom(str);
